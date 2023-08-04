455754f050b9265711093f7008b704dedb33ba30
Ankush H V <anshhv2003@gmail.com>
2023-08-04 09:24:51 +0530

    Create group_anagrams.cpp

---
+++ DSA Practice/group_anagrams.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> hash;
+        for(string x: strs) {
+            string t = x;
+            sort(t.begin(), t.end());
+            hash[t].emplace_back(x);
+        }
+        vector<vector<string>> res;
+        for(pair<string, vector<string>> x: hash) {
+            res.emplace_back(x.second);
+        }
+        return res;
+    }
+};
