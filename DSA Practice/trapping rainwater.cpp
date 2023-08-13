a3447cb684701e57e23d7638eeb88d17f6f5c2ae
Ankush H V <anshhv2003@gmail.com>
2023-08-13 13:30:15 +0530

    Create trapping rainwater.cpp

---
+++ DSA Practice/trapping rainwater.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+       int n = height.size() ;
+       int l = 0;
+       int r = n - 1;
+       int lmax = 0, rmax = 0;
+       int water = 0;
+       while(l < r) {
+           if(height[l] <= height[r]) {
+               if(height[l] > lmax) {
+                   lmax = height[l];
+               } else {
+                   water += lmax - height[l];
+               }
+               l++;
+           } else {
+               if(height[r] > rmax) {
+                   rmax = height[r];
+               } else {
+                   water += rmax - height[r];
+               }
+               r--;
+           }
+       }
+       return water;
+    }
+};
